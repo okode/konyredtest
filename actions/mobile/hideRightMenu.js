06dda6c4bfaf8aee5919d0eb36573886c7b248af
Ricardo Gil Alcañiz <rgil@okode.com>
2015-02-15 00:16:26 +0100

    Adding new resources

---
+++ actions/mobile/hideRightMenu.js
@@ -0,0 +1,17 @@
+
+
+function AS_NamedActions_10093cebe584499eb2d9ba8067e27863() {
+//Generated code
+
+
+
+
+(function ADD_SNIPPET_NamedActions_b8738426407e4e5dacda11ff914e6158() {
+frmHome["headers"][2]["isVisible"] = false;
+})();
+
+
+
+
+}
+
---
+++ actions/mobile/toggleRightMenu.js
@@ -0,0 +1,25 @@
+
+
+function AS_NamedActions_09a7984aee67491aae4a607f6b025d8e() {
+//Generated code
+
+
+
+
+AS_NamedActions_51a7d2d4a2d34571a034c35b4ca3b94a();
+
+
+
+
+
+
+(function ADD_SNIPPET_NamedActions_018346abe4c44de59afa4da2affd7d82() {
+var currentState = frmHome["headers"][2]["isVisible"];
+frmHome["headers"][2]["isVisible"] = !currentState;
+})();
+
+
+
+
+}
+
